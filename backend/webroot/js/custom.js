ed1256d457a8c1ca071db3a137a3881e4a0b042b
Nicholas Lash <nlash@nerdery.com>
2017-10-05 17:47:43 -0500

    add ability to create jokes on web front end.

--- backend/webroot/js/custom.js
+++ backend/webroot/js/custom.js
@@ -25,6 +25,19 @@ function put(id) {
     makeRequest(id, "/jokes/" + id, "PUT");
 }
 
+function post() {
+    toggleAddingControls(false);
+    makeRequest(-1, "/jokes", "POST");
+}
+
+function create() {
+    toggleAddingControls(true);
+}
+
+function cancel() {
+    toggleAddingControls(false);
+}
+
 function toggleEditingControls(show, id) {
     var setupField = document.getElementById('setup_' + id);
     var punchlineField = document.getElementById('punchline_' + id);
@@ -42,6 +55,26 @@ function toggleEditingControls(show, id) {
     saveButton.style.display = show ? "block": "none";
 }
 
+function toggleAddingControls(show) {
+    var setupField = document.getElementById('setup_create');
+    var punchlineField = document.getElementById('punchline_create');
+    var editButton = document.getElementById('add_button');
+    var saveButton = document.getElementById('save_button_create');
+    var createDivider = document.getElementById('create_divider');
+
+    
+    setupField.style.display = show ? "block" : "none"
+    punchlineField.style.display = show ? "block" : "none"
+    createDivider.style.display = show ? "block" : "none"
+
+    saveButton.style.display = show ? "block" : "none"
+    editButton.style.display = show ? "none" : "block"
+    
+    if (show) {
+        setupField.value = ""
+        punchlineField.value = ""
+    }
+}
 
 // helper functions
 function makeRequest(id, url, method) {
@@ -49,8 +82,10 @@ function makeRequest(id, url, method) {
     xhr.open(method, url, true);
     xhr.setRequestHeader("Content-Type", "application/json");
     xhr.addEventListener("load", transferComplete)
-    if (method === "PUT" || method || "POST") {
+    if (method === "PUT") {
         xhr.send(getJSONData(id));
+    } else if (method === "POST") {
+        xhr.send(getCreateJSONData());
     } else {
         xhr.send()
     }
@@ -62,6 +97,13 @@ function getJSONData(id) {
     var votesField = document.getElementById('votes_' + id);
     
     return JSON.stringify({ 'id': id, 'setup': setupField.value, 'punchline': punchlineField.value, 'votes': parseInt(votesField.value)});
+}
+
+function getCreateJSONData(id) {
+    var setupField = document.getElementById('setup_create');
+    var punchlineField = document.getElementById('punchline_create');
+    
+    return JSON.stringify({ 'setup': setupField.value, 'punchline': punchlineField.value, 'votes': 0});
 }
 
 function transferComplete(evt) {
