f47a740a397d75d58c694b2ee6a09ec99ae48794
Nicholas Lash <nlash@nerdery.com>
2017-10-05 16:59:26 -0500

    add edit, put, delete

--- backend/webroot/js/custom.js
+++ backend/webroot/js/custom.js
@@ -1,28 +1,62 @@
-function voteUp (id) {
+function voteUp(id) {
     vote(id, 1);
 }
 
-function voteDown (id) {
+function voteDown(id) {
     vote(id, -1);
 }
 
 function vote(id, change) {
     var votesField = document.getElementById('votes_' + id);
     votesField.value = parseInt(votesField.value) + change;
-    makeRequest(id);
+    makeRequest(id, "/jokes/" + id, "PUT");
 }
 
-function makeRequest (id) {
+function deleteJoke(id) {
+    makeRequest(id, "/jokes/" + id, "DELETE");
+}
+
+function edit(id) {
+    toggleEditingControls(true, id);
+}
+
+function put(id) {
+    toggleEditingControls(false, id);
+    makeRequest(id, "/jokes/" + id, "PUT");
+}
+
+function toggleEditingControls(show, id) {
+    var setupField = document.getElementById('setup_' + id);
+    var punchlineField = document.getElementById('punchline_' + id);
+    var setupText = document.getElementById('setup_text_' + id);
+    var punchlineText = document.getElementById('punchline_text_' + id);
+    var editButton = document.getElementById('edit_button_' + id);
+    var saveButton = document.getElementById('save_button_' + id);
+    
+    setupText.style.display = show ? "none" : "block";
+    punchlineText.style.display = show ? "none" : "block";
+    editButton.style.display = show ? "none" : "block";
+    
+    setupField.style.display = show ? "block": "none";
+    punchlineField.style.display = show ? "block": "none";
+    saveButton.style.display = show ? "block": "none";
+}
+
+
+// helper functions
+function makeRequest(id, url, method) {
     var xhr = new XMLHttpRequest();
-    var url = "/jokes/" + id;
-    xhr.open("PUT", url, true);
+    xhr.open(method, url, true);
     xhr.setRequestHeader("Content-Type", "application/json");
     xhr.addEventListener("load", transferComplete)
-    xhr.send(getJSONData(id));
+    if (method === "PUT" || method || "POST") {
+        xhr.send(getJSONData(id));
+    } else {
+        xhr.send()
+    }
 }
 
-function getJSONData (id) {
-    var form = document.getElementById('form_modify_' + id);
+function getJSONData(id) {
     var setupField = document.getElementById('setup_' + id);
     var punchlineField = document.getElementById('punchline_' + id);
     var votesField = document.getElementById('votes_' + id);
