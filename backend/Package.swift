f68c3e3ed8a89d9915ef56fdd7a25e7571746e8c
Nicholas Lash <nlash@nerdery.com>
2017-10-05 15:06:01 -0500

    made start of web front end.

--- backend/Package.swift
+++ backend/Package.swift
@@ -7,5 +7,6 @@ let package = Package(
 	dependencies: [
 		.Package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", majorVersion: 3),
 		.Package(url: "https://github.com/SwiftORM/SQLite-StORM", majorVersion: 3),
+		.Package(url: "https://github.com/PerfectlySoft/Perfect-Mustache.git", majorVersion: 3),
 	]
 )
