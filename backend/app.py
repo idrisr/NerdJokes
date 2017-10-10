4ef57efe5dfea011589d2b2c1a296f6e12f009ef
Id Raja <iraja@nerdery.com>
2017-10-10 17:15:27 -0500

    add the request types

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,24 @@
 from flask import Flask
+from flask import request
+app = Flask(__name__)
 
-NerdJokesApp = Flask(__name__)
 
-
-@NerdJokesApp.route("/")
-def hello():
+@app.route("/")
+def root():
     return "YO"
 
+
+#  GET /jokes // Get all jokes
+#  POST /jokes // Add a joke
+@app.route("/jokes", methods=['GET', 'POST'])
+def jokes():
+    return 'jokes %s' % request.method
+
+
+@app.route("/jokes/<int:id>", methods=['GET', 'PUT', 'DELETE'])
+def jokes_id(id):
+    return 'joke %s %s' % (id, request.method,)
+
+
 if __name__ == '__main__':
-    NerdJokesApp.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0', port=80)
