9aabdb1e9159b6b8eb286ff6e3458c031f901e27
Id Raja <iraja@nerdery.com>
2017-10-12 12:15:25 -0500

    get object from database and create json

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,19 @@
 from flask import Flask
 from flask import request
-from pystache import render
+# from pystache import render
+import sqlite3 as sqlite
+
+
 app = Flask(__name__)
 
 
 @app.route("/")
 def root():
+    conn = sqlite.connect('jokes.db')
+    c = conn.cursor()
+    for r in c.execute('select * from jokes;'):
+        joke = Joke(r)
+        print joke.toJson()
     return "YO"
 
 
@@ -19,6 +27,21 @@ def jokes():
 @app.route("/jokes/<int:id>", methods=['GET', 'PUT', 'DELETE'])
 def jokes_id(id):
     return 'joke %s %s' % (id, request.method,)
+
+
+class Joke(object):
+    def __init__(self, result):
+        self.id = result[0]
+        self.setup = result[1]
+        self.punchline = result[2]
+        self.votes = result[3]
+        self.created_time = result[4]
+        self.updated_time = result[5]
+        self.deleted_time = result[6]
+        self.uuid = result[7]
+
+    def toJson(self):
+        return 'uuid: {uuid}, setup: {setup}, punchline: {punchline}, votes: {votes}'.format(**vars(self))
 
 
 if __name__ == '__main__':
