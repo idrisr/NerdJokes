822d9f6ee9e56f64dc8dc1f0c58c89fdeea5b90d
Id Raja <iraja@nerdery.com>
2017-10-16 14:38:21 -0500

    temp throwing in the towel about modules and packages

--- backend/server/app.py
+++ backend/server/app.py
@@ -11,8 +11,6 @@ from flask import render_template
 from flask import request
 from flask import send_from_directory
 
-#  from joke import Joke
-
 import os.path
 import sqlite3 as sqlite
 import sys
@@ -80,6 +78,80 @@ def root():
     #  jokes.append(Joke(r))
 
     return render_template('index.html', jokes=jokes)
+
+
+class Joke(object):
+    table = "jokes"
+
+    def __init__(self, result):
+        self.id = result[0]
+        self.setup = result[1]
+        self.punchline = result[2]
+        self.votes = result[3]
+        self.created_time = result[4]
+        self.updated_time = result[5]
+        self.deleted_time = result[6]
+        self.uuid = result[7]
+
+    @classmethod
+    def get(cls, id):
+        query = "SELECT * FROM {} WHERE id = {}"
+        query.format(cls.table, id)
+        print(query)
+
+    #  #  todo: reuse the connection
+    #  conn = sqlite.connect('jokes.db')
+    #  c = conn.cursor()
+
+    #  request_json = request.get_json()
+
+    #  if request.method == "PUT":
+        #  vote_change = request_json["votes"]
+        #  # todo: update the update time
+        #  update_query = "UPDATE jokes SET votes = {} WHERE id = {}"\
+        #  .format(vote_change, id)
+        #  c.execute(update_query) conn.commit()
+
+    #  elif request.method == "DELETE":
+        #  # todo: set the delete flag, dont actually delete
+        #  delete_query = "DELETE FROM jokes WHERE id = {}".format(id)
+        #  c.execute(delete_query)
+        #  conn.commit()
+        #  conn.close()
+        #  return ('', httplib.NO_CONTENT)
+
+    #  if request.method == "PUT" or request.method == "GET":
+        #  select_query = "SELECT * FROM jokes WHERE id = {}".format(id)
+        #  joke = Joke(c.execute(select_query)[0])
+        #  conn.close()
+        #  return joke
+
+
+
+class QueryHelper(object):
+    def __init__(self, database):
+        self.datebase = database
+        self.connection = sqlite.connect(database)
+        self.cursor = connection.cursor()
+
+    @classmethod
+    def update(query):
+        pass
+
+    @classmethod
+    def delete(cls, query):
+        pass
+
+    @classmethod
+    def select(cls, query):
+        pass
+
+    @classmethod
+    def insert(cls, query):
+        pass
+
+    @classmethod _execute_query(query):
+        pass
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
