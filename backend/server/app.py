e314b13fa20030409a753c258e4fbc2eb1568795
Id Raja <iraja@nerdery.com>
2017-10-16 09:04:04 -0500

    refactor some shite

--- backend/server/app.py
+++ backend/server/app.py
@@ -1,16 +1,48 @@
+#!/usr/bin/env python3
+
+'''
+File: app.py
+Author: idraja
+Description: flask rest api endpoint for admin page and mobile clients
+'''
+
 from flask import Flask
 from flask import render_template
 from flask import request
 from flask import send_from_directory
 
-from joke import Joke
+#  from joke import Joke
 
-import httplib
 import os.path
 import sqlite3 as sqlite
 import sys
 
 app = Flask(__name__)
+
+
+@app.route("/jokes/<int:id>", methods=['GET'])
+def get_joke(id):
+    """get joke by id """
+    #  Joke.get(id)
+    pass
+
+
+@app.route("/jokes/<int:id>", methods=['PUT'])
+def update_joke(id):
+    """update joke by id """
+    pass
+
+
+@app.route("/jokes/", methods=['POST'])
+def create_joke():
+    """create joke """
+    pass
+
+
+@app.route("/jokes/<int:id>", methods=['DELETE'])
+def delete_joke(id):
+    """delete joke by id"""
+    pass
 
 
 @app.route('/fonts/<path:path>')
@@ -28,6 +60,13 @@ def send_css(path):
     return send_from_directory('css', path)
 
 
+@app.route("/jokes", methods=['GET'])
+def get_jokes():
+    """get all jokes"""
+    # todo:  redirect to /
+    return 'jokes %s' % request.method
+
+
 @app.route("/", methods=['GET'])
 def root():
     if not os.path.isfile('jokes.db'):
@@ -37,49 +76,10 @@ def root():
     c = conn.cursor()
     jokes = []
 
-    for r in c.execute('select * from jokes;'):
-        jokes.append(Joke(r))
+    #  for r in c.execute('select * from jokes;'):
+    #  jokes.append(Joke(r))
 
     return render_template('index.html', jokes=jokes)
-
-
-#  GET /jokes // Get all jokes
-#  POST /jokes // Add a joke
-@app.route("/jokes", methods=['GET', 'POST'])
-def jokes():
-    return 'jokes %s' % request.method
-
-
-@app.route("/jokes/<int:id>", methods=['GET', 'PUT', 'DELETE'])
-def jokes_id(id):
-    #  todo: reuse the connection
-    conn = sqlite.connect('jokes.db')
-    c = conn.cursor()
-
-    request_json = request.get_json()
-
-    if request.method == "PUT":
-        vote_change = request_json["votes"]
-        # todo: update the update time
-        update_query = "UPDATE jokes SET votes = {} WHERE id = {}"\
-            .format(vote_change, id)
-        c.execute(update_query)
-        conn.commit()
-
-    elif request.method == "DELETE":
-        # todo: set the delete flag, dont actually delete
-        delete_query = "DELETE FROM jokes WHERE id = {}".format(id)
-        c.execute(delete_query)
-        conn.commit()
-        conn.close()
-        return ('', httplib.NO_CONTENT)
-
-    if request.method == "PUT" or request.method == "GET":
-        select_query = "SELECT * FROM jokes WHERE id = {}".format(id)
-        joke = Joke(c.execute(select_query)[0])
-        conn.close()
-        return joke
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
