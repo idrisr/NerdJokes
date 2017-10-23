2beefe808111cd9e74a253d6c2c82c9f4a2ad44e
Id Raja <iraja@nerdery.com>
2017-10-23 14:28:09 -0500

    add an icon

---
+++ app/NerdJokes2/NerdJokes2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NerdJokes2
+//
+//  Created by Id Raja on 10/23/17.
+//  Copyright © 2017 Id Raja. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
