356aae434d6a16e86a79d0bceaacf7239359f6ab
Id Raja <iraja@nerdery.com>
2017-10-26 11:49:38 -0500

    delete a ton of stuff

--- app/NerdJokes/NerdJokes/AppDelegate.swift
+++ app/NerdJokes/NerdJokes/AppDelegate.swift
@@ -13,25 +13,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
-        if UserDefaults.standard.object(forKey: AppConstants.Keys.kTheme) == nil {
-            CurrentTheme.value = AppearanceManager.Theme.light.rawValue
-        }
-        
-        AppearanceManager.setupTheme(theme: AppearanceManager.Theme(rawValue: CurrentTheme.value)!)
-
-        guard
-            let navigationViewController = window?.rootViewController as? UINavigationController,
-            let jokesMasterViewController = navigationViewController.topViewController as? JokesMasterViewController else {
-                return true
-        }
-        
-        jokesMasterViewController.jokeService = JokeService(persistence: JokePersistenceService(coreDataStack: CoreDataStack()), network: JokeNetworkService())
-        
-        
-        
         return true
     }
 }
---
+++ app/NerdJokes/NerdJokes/View Controllers/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  NerdJokes
+//
+//  Created by Id Raja on 10/26/17.
+//  Copyright © 2017 Id Raja. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
